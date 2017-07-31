c531e0f5d42d1bb42f65089d3d5707a503f1c41a
Dan Parkes <daniel.parkes901@gmail.com>
2017-07-31 14:13:33 +0100

    added twitter feed

--- src/components/Careers.js
+++ src/components/Careers.js
@@ -2,8 +2,29 @@ import React from 'react';
 
 const Careers =  function() {
       return (
-        <div>
+        <div id="about">
           <img src={('./Driver-Job-Alert.jpg')}/>
+          <p>
+            Ever fancied working with the likes of Helena Bonham Carter, Patrick Stewart, Tom Hardy, Robbie Coltrane, Dame Judy Dench, Christopher Eccleston or Sheridan Smith? Working with HNA Facilities, you can experience the magic of television and film production from behind the scenes, and get paid.
+            </p>
+            <p>
+            As a leading supplier of location vehicles to the Film and TV industry, we are always looking to grow our on-location teams. Do you have what it takes?
+            </p>
+            <p>
+            If you’ve got enthusiasm, extensive driving experience with large vehicles and trailers, a full clean licence, DCPC entitlement (or a Class I/ Class II licence) and a friendly, personal demeanour, we might be able to get you started right away.
+            </p>
+            <p>
+            What the role includes:
+            </p>
+            <p>
+            As the face of HNA, our drivers and team captains are expected to represent HNA in the most professional manner at all times. Once you’ve been trained up, your role will include staying with the trailers for 5 or 6 days a week for the duration of the shoot, adhering to the needs to production and liaising with different departments. But be warned, filming hours are long but gratifying.
+            </p>
+            <p>
+            We operate from our Manchester headquarters to service productions throughout the UK and Europe. We also have regular requirements for shunting work up and down the country (evenings / nights and weekend vehicle moves on an ad hoc basis that do not require starting or ending in Manchester).
+            </p>
+            <p>
+            If we’ve still got your attention, please fill in your details below:
+            </p>
         </div>
       );
     };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Timeline } from 'react-twitter-widgets';
 
 const Home = () => {
       return (
-        <div>
+        <div className='pageheader'>
+          <div className='row'>
+            <div className="col-md-6">
           <h2>Welcome To HNA Facilities</h2>
           <p>Since supplying our first trailers to ITV’s successful crime drama, Cracker in 1992, HNA has built up an extensive track record providing facilities vehicles to TV and Film throughout the UK and mainland Europe.
           </p>
@@ -11,7 +14,21 @@ const Home = () => {
           </p>
           <p>
           HNA operates a comprehensive range of vehicles to cater for all requirements, large or small, using experienced, friendly drivers. Our hands-on 24/7 management approach ensures your job runs smoothly every time.</p>
-          
+          </div>
+          <div  className="col-md-6">
+            <Timeline
+              dataSource={{
+                sourceType: 'profile',
+                screenName: 'hnafacilities'
+              }}
+             className="text-right"
+              options={{
+                username: 'HNA Facilities',
+                height: '1000'
+              }}
+            />
+            </div>
+            </div>
         </div>
       );
     };
